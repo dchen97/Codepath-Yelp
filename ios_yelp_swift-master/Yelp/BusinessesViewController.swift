e96cc733a6aa6f42c398ed6e4987ea6d62eb57b3
Diana <crystaljerry@gmail.com>
2017-02-21 01:27:24 -0700

    Add required features

--- ios_yelp_swift-master/Yelp/BusinessesViewController.swift
+++ ios_yelp_swift-master/Yelp/BusinessesViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating,  UISearchBarDelegate, UISearchControllerDelegate {
+    @IBOutlet weak var tableView: UITableView!
     
     var businesses: [Business]!
+    
+    var searchController: UISearchController!
+    
+    var filteredBusinesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 120
+        
+        self.searchController = UISearchController(searchResultsController: nil)
+        self.searchController.delegate = self
+        self.searchController.searchResultsUpdater = self
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = false
+        self.searchController.searchBar.barTintColor = UIColor.red
+        self.searchController.searchBar.barTintColor = UIColor.red
+        self.searchController.searchBar.tintColor = UIColor.white
+        self.searchController.searchBar.placeholder = "Restaurants"
+        self.searchController.searchBar.sizeToFit()
+        
+        self.navigationItem.titleView = searchController.searchBar
+        self.navigationController?.navigationBar.barTintColor = UIColor.red
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.backgroundColor = UIColor.red
+        }
+        
+        definesPresentationContext = true
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.filteredBusinesses = businesses
+            self.tableView.reloadData()
+            
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -41,10 +74,44 @@ class BusinessesViewController: UIViewController {
         
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if filteredBusinesses != nil {
+            return filteredBusinesses!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Business Cell", for: indexPath) as! BusinessCell
+        
+        cell.business = filteredBusinesses[indexPath.row]
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filteredBusinesses = searchText.isEmpty ? businesses : businesses.filter({(business: Business) -> Bool in
+                return business.name!.range(of: searchText, options: .caseInsensitive) != nil
+            })
+            
+            tableView.reloadData()
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+//    - (UIStatusBarStyle) preferredStatusBarStyle {
+//    return UIStatusBarStyleLightContent;
+//    }
     
     /*
      // MARK: - Navigation
